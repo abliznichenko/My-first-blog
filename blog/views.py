676f00013c411e40feba104f75f24f917e95d418
abliznichenko <albliznichenko@gmail.com>
2022-09-08 17:06:07 +0300

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
 from django.shortcuts import render
 
-# Create your views here.
+def display(request):
+    return render(request, 'template.tmpl', {'obj':models.Book.objects.all()})
